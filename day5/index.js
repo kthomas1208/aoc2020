63b55835de922e4525fc6570f5f448b72de093da
Kevin Thomas <kthomas1208@gmail.com>
2019-12-09 23:07:20 -0600

    day5 attempt 1

--- day5/index.js
+++ day5/index.js
@@ -1,3 +1,110 @@
-function day5() {}
+var OpCodes = {
+  ADD: 1,
+  MULTIPLY: 2,
+  INPUT: 3,
+  OUTPUT: 4,
+  HALT: 99
+};
+
+function day5(instruction) {
+  const userInput = 1;
+  let cursor = 0;
+  while (cursor < instruction.length) {
+    let opcode = instruction[cursor];
+    switch (opcode) {
+      case OpCodes.ADD:
+        add(instruction, cursor);
+        cursor += 4;
+        break;
+      case OpCodes.MULTIPLY:
+        multiply(instruction, cursor);
+        cursor += 4;
+        break;
+      case OpCodes.INPUT:
+        input(instruction, cursor, userInput);
+        cursor += 2;
+        break;
+      case OpCodes.OUTPUT:
+        output(instruction, cursor);
+        cursor += 2;
+        break;
+      case OpCodes.HALT:
+        return;
+      default:
+        console.log(`Parameter Mode: ${opcode}`);
+        parameterMode(instruction, cursor);
+        cursor += 4;
+        break;
+    }
+  }
+  return instruction;
+}
+
+function add(instruction, cursor) {
+  const in1 = instruction[cursor + 1];
+  const in2 = instruction[cursor + 2];
+  const out = instruction[cursor + 3];
+
+  instruction[out] = instruction[in1] + instruction[in2];
+}
+
+function multiply(instruction, cursor) {
+  const in1 = instruction[cursor + 1];
+  const in2 = instruction[cursor + 2];
+  const out = instruction[cursor + 3];
+
+  instruction[out] = instruction[in1] * instruction[in2];
+}
+
+function input(instruction, cursor, userInput) {
+  instruction[cursor + 1] = userInput;
+}
+
+function output(instruction, cursor) {
+  console.log(`Test Result: ${instruction[cursor + 1]}`);
+}
+
+function parameterMode(instruction, cursor) {
+  const in0 = instruction[cursor].toString();
+  const opCode = Number(in0.slice(-2));
+  const in1Mode = Number(in0.slice(-3, -2));
+  const in2Mode = Number(in0.slice(-4, -3));
+
+  const in1 = in1Mode
+    ? instruction[cursor + 1]
+    : instruction[instruction[cursor + 1]];
+  const in2 = in2Mode
+    ? instruction[cursor + 2]
+    : instruction[instruction[cursor + 2]];
+  const out = instruction[cursor + 3];
+
+  if (opCode === OpCodes.ADD) {
+    instruction[out] = in1 + in2;
+  } else if (opCode === OpCodes.MULTIPLY) {
+    instruction[out] = in1 * in2;
+  }
+}
+
+// function day2(input) {
+//   for (let i = 0; i + 4 < input.length; i += 4) {
+//     let instruction = input.slice(i, i + 4);
+//     processInstruction(instruction, input);
+//   }
+
+//   return input;
+// }
+
+// function processInstruction(instruction, input) {
+//   const opcode = instruction[0];
+//   const in1 = instruction[1];
+//   const in2 = instruction[2];
+//   const out = instruction[3];
+
+//   if (opcode === 1) {
+//     input[out] = input[in1] + input[in2];
+//   } else if (opcode === 2) {
+//     input[out] = input[in1] * input[in2];
+//   } else if (opcode === 99) return;
+// }
 
 module.exports = { day5 };
