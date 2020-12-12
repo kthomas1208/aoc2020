806b2dc7865bce9ee87901f0de3cab2e1980ccb5
Kevin Thomas <kevinthomas@Kevins-MBP.localdomain>
2020-12-11 22:09:04 -0600

    doin a SOLID for day 11

--- day11/index.ts
+++ day11/index.ts
@@ -72,135 +72,6 @@ const countOccupied = function (grid: any[]): number {
     return count;
 }
 
-/**
- * Okay, this is really bad because I should be using recursion or something. 
- * Also, I'm sorry for making x vertical and y horizontal
- * @param row 
- * @param col 
- * @param grid 
- */
-const checkOccupiedAdvanceWars = function (row: number, col: number, grid: any[]): number {
-    let totalCount = 0;
-
-    // top left
-    let x = row - 1, y = col - 1;
-    while (x >= 0 && y >= 0) {
-        if (grid[x] === undefined) break;
-        if (grid[x][y] === undefined) break;
-        if (grid[x][y] === '#') {
-            totalCount++;
-            break;
-        } else if (grid[x][y] === 'L') {
-            break;
-        }
-
-        x--; y--;
-    }
-
-    // top
-    for (let x = row - 1; x >= 0; x--) {
-        if (grid[x] === undefined) break;
-        if (grid[x][col] === '#') {
-            totalCount++;
-            break;
-        } else if (grid[x][col] === 'L') {
-            break;
-        }
-    }
-
-    // top right
-    x = row - 1, y = col + 1;
-    while (x >= 0 && y < grid[0].length) {
-        if (grid[x] === undefined) break;
-        if (grid[x][y] === undefined) break;
-        if (grid[x][y] === '#') {
-            totalCount++;
-            break;
-        } else if (grid[x][y] === 'L') {
-            break;
-        }
-
-        x--; y++;
-    }
-
-    // left
-    for (let y = col - 1; y >= 0; y--) {
-        if (grid[row][y] === undefined) break;
-        if (grid[row][y] === '#') {
-            totalCount++;
-            break;
-        } else if (grid[row][y] === 'L') {
-            break;
-        }
-    }
-
-    // bottom left
-    x = row + 1, y = col - 1;
-    while (x < grid.length && y >= 0) {
-        if (grid[x] === undefined) break;
-        if (grid[x][y] === undefined) break;
-        if (grid[x][y] === '#') {
-            totalCount++;
-            break;
-        } else if (grid[x][y] === 'L') {
-            break;
-        }
-
-        x++; y--;
-    }
-
-    // bottom
-    for (let x = row + 1; x < grid.length; x++) {
-        if (grid[x] === undefined) break;
-        if (grid[x][col] === '#') {
-            totalCount++;
-            break;
-        } else if (grid[x][col] === 'L') {
-            break;
-        }
-    }
-
-    // bottom right
-    x = row + 1, y = col + 1;
-    while (x < grid.length && y < grid[0].length) {
-        if (grid[x] === undefined) break;
-        if (grid[x][y] === undefined) break;
-        if (grid[x][y] === '#') {
-            totalCount++;
-            break;
-        } else if (grid[x][y] === 'L') {
-            break;
-        }
-
-        x++; y++;
-    }
-
-    // right
-    for (let y = col + 1; y < grid[0].length; y++) {
-        if (grid[row][y] === undefined) break;
-        if (grid[row][y] === '#') {
-            totalCount++;
-            break;
-        } else if (grid[row][y] === 'L') {
-            break;
-        }
-    }
-
-    return totalCount;
-}
-
-const findOccupiedSeat = function (x: number, y: number, grid: any[]): number {
-    if (grid[x] === undefined) return -1;
-    if (grid[x][y] === undefined) return -1;
-    if (grid[x][y] === '#') {
-        return 1;
-    } else if (grid[x][y] === 'L') {
-        return -1;
-    } else {
-        return 0;
-    }
-}
-
 const day11_2 = function (input: string[]): number {
 
     let seats = [];
@@ -232,6 +103,131 @@ const day11_2 = function (input: string[]): number {
         if (!changed) {
             return countOccupied(seats);
         }
+    }
+}
+
+/**
+ * Okay, this is really bad because I should be using recursion or something. 
+ * Also, I'm sorry for making x vertical and y horizontal
+ * Also, I'm sorry for mixing fors and whiles
+ * @param row 
+ * @param col 
+ * @param grid 
+ */
+const checkOccupiedAdvanceWars = function (row: number, col: number, grid: any[]): number {
+    let totalCount = 0;
+
+    // top left
+    let x = row - 1, y = col - 1;
+    while (x >= 0 && y >= 0) {
+        let result = findOccupiedSeat(x, y, grid);
+        if (result === 1) {
+            totalCount++;
+            break;
+        } else if (result === -1) {
+            break;
+        }
+        x--; y--;
+    }
+
+    // top
+    for (let x = row - 1; x >= 0; x--) {
+        let result = findOccupiedSeat(x, col, grid);
+        if (result === 1) {
+            totalCount++;
+            break;
+        } else if (result === -1) {
+            break;
+        }
+    }
+
+    // top right
+    x = row - 1, y = col + 1;
+    while (x >= 0 && y < grid[0].length) {
+        let result = findOccupiedSeat(x, y, grid);
+        if (result === 1) {
+            totalCount++;
+            break;
+        } else if (result === -1) {
+            break;
+        }
+
+        x--; y++;
+    }
+
+    // left
+    for (let y = col - 1; y >= 0; y--) {
+        let result = findOccupiedSeat(row, y, grid);
+        if (result === 1) {
+            totalCount++;
+            break;
+        } else if (result === -1) {
+            break;
+        }
+    }
+
+    // bottom left
+    x = row + 1, y = col - 1;
+    while (x < grid.length && y >= 0) {
+        let result = findOccupiedSeat(x, y, grid);
+        if (result === 1) {
+            totalCount++;
+            break;
+        } else if (result === -1) {
+            break;
+        }
+
+        x++; y--;
+    }
+
+    // bottom
+    for (let x = row + 1; x < grid.length; x++) {
+        let result = findOccupiedSeat(x, col, grid);
+        if (result === 1) {
+            totalCount++;
+            break;
+        } else if (result === -1) {
+            break;
+        }
+    }
+
+    // bottom right
+    x = row + 1, y = col + 1;
+    while (x < grid.length && y < grid[0].length) {
+        let result = findOccupiedSeat(x, y, grid);
+        if (result === 1) {
+            totalCount++;
+            break;
+        } else if (result === -1) {
+            break;
+        }
+
+        x++; y++;
+    }
+
+    // right
+    for (let y = col + 1; y < grid[0].length; y++) {
+        let result = findOccupiedSeat(row, y, grid);
+        if (result === 1) {
+            totalCount++;
+            break;
+        } else if (result === -1) {
+            break;
+        }
+    }
+
+    return totalCount;
+}
+
+const findOccupiedSeat = function (x: number, y: number, grid: any[]): number {
+    if (grid[x] === undefined) return -1;
+    if (grid[x][y] === undefined) return -1;
+    if (grid[x][y] === '#') {
+        return 1;
+    } else if (grid[x][y] === 'L') {
+        return -1;
+    } else {
+        return 0;
     }
 }
 
